707243bb7a39e17572b183cd94e366f3694f2cab
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-01-29 17:21:05 -0500

    New features wooh!
    New features / fixes:
    - Added get_user_choice function. So, if the user inputs an invalid choice, the code will ask them the question prompts again to retry.
    - Fixed a bug caused by the Ontario gov't now giving London covid numbers in floats rather than integers. New code tells pyplot to compensate for this.
    - Some code reorganization

--- covid-scraper-and-graphing.py
+++ covid-scraper-and-graphing.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup as soup
 from csv import reader
 from csv import writer
 from matplotlib import pyplot
@@ -10,13 +9,14 @@ import numpy as np
 csv_url = 'https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/8a88fe6d-d8fb-41a3-9d04-f0550a44999f/download/daily_change_in_cases_by_phu.csv'
 
 
-
+# Fetches data from Ontario gov't servers -> Writes it into a CSV file.
 def get_write_data():
     r = requests.get(csv_url)
     with open("ontario_covid.csv", 'wb') as f:
         f.write(r.content)
         f.close()
-        
+
+# Getting London case data from 'Q' Column in the CSV file. Runs through numpy to parse it so that the graph can read it as an integer (seen below).
 def plot_cases_london():
     with open("ontario_covid.csv", 'r') as f:
         data = list(reader(f))
@@ -36,55 +36,54 @@ def plot_cases_ontario():
     return numpy_cases
 
    
-
 def plot_dates():
     with open("ontario_covid.csv", 'r') as e:
         data = list(reader(e))
         dates = [parser.parse(i[0]) for i in data[7::]]
         return dates
     
+def get_choice():    
+    u_choice = input("What do you want to do?\n (a) Just update and write data to the csv\n (b) Update data AND graph London cases\n (c) Update data AND graph Ontario cases\n")
+
+    if u_choice == 'a':
+        get_write_data()
+        print("Updated csv!")
+
+    elif u_choice == 'b':
+        get_write_data()
+
+        dates = plot_dates()
+        numpy_cases = plot_cases_london()
     
-    
+        print(numpy_cases)
 
-u_choice = input("What do you want to do?\n (a) Just update and write data to the csv\n (b) Update data AND graph London cases\n (c) Update data AND graph Ontario cases\n")
+        pyplot.bar(dates, numpy_cases.astype(float), color='blue') # Modified from pyplot.bar(dates, numpy_cases.astype(int)) to this because the Ontario gov't began providing London numbers in floats. 
 
-if u_choice == 'a':
-    get_write_data()
-    print("Updated csv!")
+        pyplot.title('Holy macaroni - London')
+        pyplot.xlabel('Date (in months)')
+        pyplot.ylabel('Case numbers')
+        pyplot.show()
 
-elif u_choice == 'b':
-    get_write_data()
+    elif u_choice == 'c':
+        get_write_data()
 
-    dates = plot_dates()
-    numpy_cases = plot_cases_london()
-    print(numpy_cases)
+        dates = plot_dates()
+        numpy_cases = plot_cases_ontario()
 
-    pyplot.bar(dates, numpy_cases.astype(int))
+        print(numpy_cases)
 
-    pyplot.title('Holy macaroni - London')
-    pyplot.xlabel('Date (in months)')
-    pyplot.ylabel('Case numbers')
-    pyplot.show()
+        pyplot.bar(dates, numpy_cases.astype(int), color='green')
 
-    print("Finished!")
+        pyplot.title('Holy macaroni - Ontario')
+        pyplot.xlabel('Date (in months)')
+        pyplot.ylabel('Case numbers')
+        pyplot.show()
 
-elif u_choice == 'c':
-    get_write_data()
+    else:
+        print("Invalid choice!")
+        get_choice() # Added so that if the user chooses an invalid option, (s)he will get sent back to the questions again to retry.
 
-    dates = plot_dates()
-    numpy_cases = plot_cases_ontario()
-    print(numpy_cases)
-
-    pyplot.bar(dates, numpy_cases.astype(int))
-
-    pyplot.title('Holy macaroni - Ontario')
-    pyplot.xlabel('Date (in months)')
-    pyplot.ylabel('Case numbers')
-    pyplot.show()
-
-
-else:
-    print("Invalid choice!")
+get_choice()
 
 
 
